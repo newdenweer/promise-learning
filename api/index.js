5666db486e3919742616338ff20c72292f72f108
newdenweer <newdenweer@mail.ru>
2021-06-01 12:26:43 +0300

    first commit

--- api/index.js
+++ api/index.js
@@ -1,2 +1,12 @@
-console.log('up');
+const Sequelize = require('sequelize');
+const models = require('./models');
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+	res.status(200).json('Server is working');
+});
+app.listen(3000, () => {
+	console.log('Server started at 3000');
+});
+console.log('up');
---
+++ api/models/index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
+	dialect: 'mysql',
+	host: process.env.DB_HOST,
+});
+
+fs.readdirSync(__dirname)
+	.filter(file => {
+		return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+	})
+	.forEach(file => {
+		const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+		db[model.name] = model;
+	});
+
+Object.keys(db).forEach(modelName => {
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
+});
+
+sequelize
+	.sync()
+	.then(result => {
+		console.log('ok');
+	})
+	.catch(err => console.log(err));
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
